bbee2eb9aecd2b3acef6b1eb15bc3f63aea87d07
Ranyaadel <raniaadel9222gmail.com>
2020-03-23 17:48:42 -0700

    Added file

---
+++ Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Food_ordering_System.Models
+{
+    public class CartItem
+    {
+        public int CartItemId { get; set; }
+        
+        public int Amount { get; set; }
+        
+
+    }
+}
